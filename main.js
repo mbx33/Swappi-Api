df658c55599470df9983958435be86ae499e1b77
Matt Baxter <mbx33@hotmail.com>
2021-08-09 22:14:47 -0600

    Second try

--- main.js
+++ main.js
@@ -1,50 +1,29 @@
 const button = document.querySelector('#button');
 
 
-
-
 function bttnClick() {
     console.log("Button was clicked")
     axios.get('https://swapi.dev/api/planets/2')
     .then(res => { 
-        console.log(res.data.residents)
         const myArr = res.data.residents
         for (let i = 0; i < myArr.length; i++){
-            console.log(myArr)
+            // console.log(myArr)
             axios.get(myArr[i])            
             .then(res => {
-                const myPeeps = res.name                
-                    // console.log(myPeeps);
-                const myHeader = document.getElementById("myPeeps").innerHTML = `${myPeeps} are from Alderaan`
-            })            
-        } 
-    })  
+                const myNewArr = res.data.name
+                const listOfPeople = document.querySelector('#myPeeps');
+                const newList = document.createElement('h4')
+                newList.innerText = myNewArr;
+
+                listOfPeople.append(newList)
+                console.log(myNewArr);                               
+            })              
+        }         
+    })      
 }
-    
-    
-        
 
+// document.getElementById("myPeeps").innerHTML = `${myNewArr} is from Alderaan`;                                              
 
-// function buttonClick() {
-//     axios.get('https://swapi.dev/api/planets/2')
-//     .then(res => {console.log(res.data.residents);
-//         // const myArr = res.data.residents
-
-//         // for (let i = 0; i < res.residents; i++){
-//         //     axios.get(myArr[i])
-//         //             .then(res => console.log(res1.data.name))
-                    // const nameDiv = document.createElement('div')
-                    // nameDiv.innerText = res1.data.name
-//                     // .catch(err => console.log(err))
-//         // }
-        
-//     })
-//     .catch(error => {
-//         console.log(error)
-//     })
-
-    
-// }
 
 
 
